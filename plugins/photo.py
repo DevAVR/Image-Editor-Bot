079b15b12e2f8ebd075e4ac75a3a7e958f479f3b
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-01 18:55:53 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -58,11 +58,11 @@ async def photo(client: Client, message: Message):
             await update.reply_text("Something Wrong. Contact my Support Group")
 
             return
-        
-        await client.send_message(
-            chat_id=message.chat.id,
-            text="Select your required mode from below!ㅤㅤ",
-            reply_markup=InlineKeyboardMarkup(
+        except Exception:
+            await client.send_message(
+                 chat_id=message.chat.id,
+                 text="Select your required mode from below!ㅤㅤ",
+                 reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton(text="BRIGHT", callback_data="bright"),
