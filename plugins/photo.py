73a1e148e1b8206955266c08a66b76c94d1c7ede
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-01 18:51:52 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -6,6 +6,61 @@ from helper_funcs.chat_base import TRChatBase
 @Client.on_message(filters.photo & filters.private)
 async def photo(client: Client, message: Message):
     try:
+        
+
+    if update.from_user.id in Config.BANNED_USERS:
+
+        await bot.send_message(
+
+            chat_id=update.chat.id,
+
+            text=Translation.BANNED_USER_TEXT,
+
+            reply_to_message_id=update.message_id
+
+        )
+
+        return
+
+    TRChatBase(update.from_user.id, update.text, "photo")
+
+    update_channel = Config.UPDATE_CHANNEL
+
+    if update_channel:
+
+        try:
+
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+
+            if user.status == "kicked":
+
+               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+
+               return
+
+        except UserNotParticipant:
+
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+
+            await update.reply_text(
+
+                text="**Join My Updates Channel to use ME 😎 🤭**",
+
+                reply_markup=InlineKeyboardMarkup([
+
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+
+              ])
+
+            )
+
+            return
+
+        except Exception:
+
+            await update.reply_text("Something Wrong. Contact my Support Group")
+
+            return
         await client.send_message(
             chat_id=message.chat.id,
             text="Select your required mode from below!ㅤㅤ",
