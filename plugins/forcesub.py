9093560644fc264e4bffabcf011ed2f3dc352381
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-01 19:56:48 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -13,20 +13,19 @@ from helper_funcs.chat_base import TRChatBase
 
 @Client.on_message(filters.photo & filters.private)
 TRChatBase(update.from_user.id, update.text, "photo")
-    update_channel = Config.UPDATE_CHANNEL
+update_channel = Config.UPDATE_CHANNEL
+if update_channel:
+  try:
+    user = await bot.get_chat_member(update_channel, update.chat.id)
+    if user.status == "kicked":
+      await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+      return
 
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-                await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-                return
-
-        except UserNotParticipant:
+except UserNotParticipant:
 
             #await update.reply_text(f"Join @{update_channel} To Use Me")
 
-            await update.reply_text(text="**Join My Updates Channel to use ME 😎 🤭**",
+  await update.reply_text(text="**Join My Updates Channel to use ME 😎 🤭**",
                                     reply_markup=InlineKeyboardMarkup([
                                         [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
                                         ])
