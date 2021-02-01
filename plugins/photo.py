77be2c4e575890be90086fdb10ff71077fb3b779
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-01 19:46:34 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -13,29 +13,6 @@ from helper_funcs.chat_base import TRChatBase
 
 @Client.on_message(filters.photo & filters.private)
 async def photo(client: Client, message: Message):
-    TRChatBase(update.from_user.id, update.text, "photo")
-    update_channel = Config.UPDATE_CHANNEL
-
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-                await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-                return
-
-        except UserNotParticipant:
-
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-
-            await update.reply_text(text="**Join My Updates Channel to use ME 😎 🤭**",
-                                    reply_markup=InlineKeyboardMarkup([
-                                        [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-                                        ])
-                                    )
-            return
-        except Exception:
-            await update.reply_text("Something Wrong. Contact my Support Group")
-            return
   TRChatBase(update.from_user.id, update.text, "photo")
     try:
         await client.send_message(
