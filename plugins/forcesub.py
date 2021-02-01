09f84bc3a9c70a7f46dbd8d71d9fc0bb8d9af578
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-01 19:47:49 +0530

    Create forcesub.py

---
+++ plugins/forcesub.py
@@ -0,0 +1,23 @@
+    TRChatBase(update.from_user.id, update.text, "photo")
+    update_channel = Config.UPDATE_CHANNEL
+
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+                await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+                return
+
+        except UserNotParticipant:
+
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+
+            await update.reply_text(text="**Join My Updates Channel to use ME 😎 🤭**",
+                                    reply_markup=InlineKeyboardMarkup([
+                                        [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                                        ])
+                                    )
+            return
+        except Exception:
+            await update.reply_text("Something Wrong. Contact my Support Group")
+            return
