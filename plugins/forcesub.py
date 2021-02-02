35bf487ca2c1e693f0720dcb692ffcb3af4a9573
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-02 10:24:16 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -6,30 +6,55 @@ logger = logging.getLogger(__name__)
 import os
 import time
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from pyrogram import Client, filters
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+from pyrogram.types import ReplyKeyboardMarkup,InlineKeyboardMarkup,InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import Client, filters
+
 from helper_funcs.chat_base import TRChatBase
+from helper_funcs.display_progress import progress_for_pyrogram
 
-@Client.on_message(filters.photo & filters.private)
-TRChatBase(update.from_user.id, update.text, "photo")
-update_channel = Config.UPDATE_CHANNEL
-if update_channel:
-  try:
-    user = await bot.get_chat_member(update_channel, update.chat.id)
-    if user.status == "kicked":
-      await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-      return
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+# https://stackoverflow.com/a/37631799/4723940
+from PIL import Image
+from database.database import *
 
-except UserNotParticipant:
 
+@pyrogram.Client.on_message(pyrogram.filters.command(["photo"]))
+    if update.from_user.id in Config.BANNED_USERS:
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.BANNED_USER_TEXT,
+            reply_to_message_id=update.message_id
+        )
+        return
+    TRChatBase(update.from_user.id, update.text, "photo")
+    update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+               return
+        except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
-
-  await update.reply_text(text="**Join My Updates Channel to use ME 😎 🤭**",
-                                    reply_markup=InlineKeyboardMarkup([
-                                        [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-                                        ])
-                                    )
+            await update.reply_text(
+                text="**Join My Updates Channel to use ME 😎 🤭**",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+              ])
+            )
             return
         except Exception:
             await update.reply_text("Something Wrong. Contact my Support Group")
